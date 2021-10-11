4f555a0fb0737845846b8fe45132ddcf3d634333
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2021-10-12 01:20:05 +0300

    Add TeamCity configs for publishing :plugin and :runtime to Space dev repository

--- .teamcity/additionalConfiguration.kt
+++ .teamcity/additionalConfiguration.kt
@@ -5,6 +5,8 @@
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildTypeSettings
+import jetbrains.buildServer.configs.kotlin.v2019_2.FailureAction
+import jetbrains.buildServer.configs.kotlin.v2019_2.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.GradleBuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
@@ -16,6 +18,8 @@ fun Project.additionalConfiguration() {
     }
 
     deployPlugin()
+
+    configureSpaceDeployments()
 }
 
 fun fastBenchmarkTasks(platform: Platform): String {
@@ -23,6 +27,9 @@ fun fastBenchmarkTasks(platform: Platform): String {
         "js", "jvm", platform.nativeTaskPrefix()
     ).joinToString(separator = " ", transform = { "${it}FastBenchmark" })
 }
+
+
+// deploy plugin to Gradle Plugin Portal
 
 const val gradlePublishKey = "gradle.publish.key"
 const val gradlePublishSecret = "gradle.publish.secret"
@@ -58,3 +65,100 @@ fun Project.deployPlugin() = BuildType {
         }
     }
 }.also { buildType(it) }
+
+
+// deploy plugin and runtime to Space maven repository
+
+fun Project.configureSpaceDeployments() {
+    val buildAll = knownBuilds.buildAll
+    val deployVersion = knownBuilds.deployVersion
+    val deploys = platforms.map { deployToSpace(it, deployVersion) }
+    val deployPublish = deployPublishToSpace(deployVersion).apply {
+        dependsOnSnapshot(buildAll, onFailure = FailureAction.IGNORE)
+        deploys.forEach {
+            dependsOnSnapshot(it)
+        }
+    }
+    deployPluginToSpace()
+}
+
+const val DEPLOY_PUBLISH_TO_SPACE_ID = "Deploy_Publish_To_Space"
+
+fun Project.deployPublishToSpace(configureBuild: BuildType) = BuildType {
+    id(DEPLOY_PUBLISH_TO_SPACE_ID)
+    this.name = "Deploy (Publish to Space)"
+    type = BuildTypeSettings.Type.COMPOSITE
+    dependsOnSnapshot(configureBuild)
+    buildNumberPattern = configureBuild.depParamRefs.buildNumber.ref
+    params {
+        // Tell configuration build how to get release version parameter from this build
+        // "dev" is the default and means publishing is not releasing to public
+        text(configureBuild.reverseDepParamRefs[releaseVersionParameter].name, "dev", display = ParameterDisplay.PROMPT, label = "Release Version")
+    }
+    commonConfigure()
+}.also { buildType(it) }
+
+const val DEPLOY_PUBLISH_PLUGIN_TO_SPACE_ID = "Deploy_Publish_Plugin_To_Space"
+
+fun Project.deployPluginToSpace() = BuildType {
+    id(DEPLOY_PUBLISH_PLUGIN_TO_SPACE_ID)
+    this.name = "Deploy (Publish Plugin To Space)"
+    commonConfigure()
+
+    requirements {
+        // Require Linux for publishing the plugin
+        contains("teamcity.agent.jvm.os.name", "Linux")
+    }
+
+    dependsOnSnapshot(this@deployPluginToSpace.knownBuilds.buildAll)
+    buildNumberPattern = this@deployPluginToSpace.knownBuilds.buildVersion.depParamRefs.buildNumber.ref
+
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    maxRunningBuilds = 1
+
+    params {
+        param("system.space.user", "abduqodiri.qurbonzoda")
+        password("system.space.token", "credentialsJSON:7aa03210-1f86-452e-b786-920f8a321b7d")
+    }
+
+    steps {
+        gradle {
+            name = "Publish Plugin"
+            jdkHome = "%env.$jdk%"
+            jvmArgs = "-Xmx1g"
+            gradleParams = "--info --stacktrace"
+            tasks = "clean :plugin:publishToSpaceRepository"
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+}.also { buildType(it) }
+
+fun Project.deployToSpace(platform: Platform, configureBuild: BuildType) = buildType("Deploy_To_Space", platform) {
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    enablePersonalBuilds = false
+    maxRunningBuilds = 1
+    params {
+        param(versionSuffixParameter, "${configureBuild.depParamRefs[versionSuffixParameter]}")
+        param(releaseVersionParameter, "${configureBuild.depParamRefs[releaseVersionParameter]}")
+        param("system.space.user", "abduqodiri.qurbonzoda")
+        password("system.space.token", "credentialsJSON:7aa03210-1f86-452e-b786-920f8a321b7d")
+    }
+
+    vcs {
+        cleanCheckout = true
+    }
+
+    steps {
+        gradle {
+            name = "Deploy ${platform.buildTypeName()} Binaries to Space"
+            jdkHome = "%env.$jdk%"
+            jvmArgs = "-Xmx1g"
+            gradleParams = "--info --stacktrace -P$versionSuffixParameter=%$versionSuffixParameter% -P$releaseVersionParameter=%$releaseVersionParameter%"
+            tasks = "clean publishToSpaceRepository"
+            buildFile = ""
+            gradleWrapperPath = ""
+        }
+    }
+}.dependsOnSnapshot(configureBuild)
